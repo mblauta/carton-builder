592fe187ab9f72f939fa75d821d97bb75a71b3af
Mel Blauta <mblauta@gmail.com>
2020-02-19 13:20:08 -0500

    Add base test project

---
+++ application/CartonBuilder.Web.Tests/Controllers/CartonControllerTest.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Web.Mvc;
+using CartonBuilder.Web.Controllers;
+using CartonBuilder.Web.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CartonBuilder.Web.Tests.Controllers
+{
+    [TestClass]
+    public class CartonControllerTest
+    {
+        private readonly int _cartonId = 1;
+        private readonly int _equipmentId = 1;
+
+        [TestMethod]
+        public void Index_Returns_ViewResult()
+        {
+            // Arrage
+            var cartonController = new CartonController();
+
+            // Act
+            var result = cartonController.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Details_Returns_ViewResult()
+        {
+            // Arrage
+            var cartonController = new CartonController();
+
+            // Act
+            var result = cartonController.Details(_cartonId) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Create_Returns_ViewResult()
+        {
+            // Arrage
+            var cartonController = new CartonController();
+
+            // Act
+            var result = cartonController.Create() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Edit_Returns_ViewResult()
+        {
+            // Arrage
+            var cartonController = new CartonController();
+
+            // Act
+            var result = cartonController.Edit(_cartonId) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Delete_ReturnsView_Result()
+        {
+            // Arrage
+            var cartonController = new CartonController();
+
+            // Act
+            var result = cartonController.Delete(_cartonId) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ListAvailableEquipment_Returns_ViewResult()
+        {
+            // Arrage
+            var cartonController = new CartonController();
+
+            // Act
+            var result = cartonController.ListAvailableEquipment(_cartonId) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void AddEquipment_Returns_ViewResult()
+        {
+            // Arrage
+            var cartonController = new CartonController();
+
+            // Act
+            var result = cartonController.AddEquipment(_cartonId, _equipmentId) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ListAddedEquipment_Returns_ViewResult()
+        {
+            // Arrage
+            var cartonController = new CartonController();
+
+            // Act
+            var result = cartonController.ListAddedEquipment(_cartonId) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void RemoveEquipment_Returns_ViewResult()
+        {
+            // Arrage
+            var cartonController = new CartonController();
+
+            // Act
+            var result = cartonController.RemoveEquipment(_cartonId, _equipmentId) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
---
+++ application/CartonBuilder.Web.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CartonBuilder.Web.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CartonBuilder.Web.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3efedb71-c52f-4086-8665-136fbb2720f2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
