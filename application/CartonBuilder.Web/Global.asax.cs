7a896f0717d2e1c4c544b51f0656656413d6f605
Mel Blauta <mblauta@gmail.com>
2020-02-19 11:06:59 -0500

    Tidy up UI

--- application/CartonBuilder.Web/Global.asax.cs
+++ application/CartonBuilder.Web/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
